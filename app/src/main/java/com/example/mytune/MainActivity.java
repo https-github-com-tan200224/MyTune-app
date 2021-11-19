6033cbe5b5bf9791050abd6925c2bbf66fc51c7e
tan200224 <tan200224@gmail.com>
2021-11-19 16:40:22 -0500

    Parse Add commit

--- app/src/main/java/com/example/mytune/MainActivity.java
+++ app/src/main/java/com/example/mytune/MainActivity.java
@@ -1,9 +1,23 @@
 package com.example.mytune;
 
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.mytune.fragments.ComposeFragment;
+import com.example.mytune.fragments.PostsFragment;
+import com.example.mytune.fragments.ProfileFragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +31,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queryPosts();
+    }
+
+
+
+    private void queryPosts() {
+        ParseQuery <Post> query = ParseQuery.getQuery(Post.class);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with Getting Posts", e);
+                    return;
+                }
+                for (Post post: posts) {
+                    Log.i(TAG, "Post" + post.getDescription());
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mytune/ParseApplication.java
+++ app/src/main/java/com/example/mytune/ParseApplication.java
@@ -1,4 +1,24 @@
 package com.example.mytune;
 
-public class ParseApplication {
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("DKolZInaX5yFsY4ah57U5wU2U20vDNYcY33vxmWu")
+                .clientKey("qDqqSTm4apLBFGIAapLOJ3qPartLihgcf4YQ1VVF")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+
+    }
 }
