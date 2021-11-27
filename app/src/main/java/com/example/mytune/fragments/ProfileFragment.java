b0776bf0971381aaf32a3ddeb50835e2cd7ef6a7
tan200224 <tan200224@gmail.com>
2021-11-27 16:57:47 -0500

    Add Botton Navigation Veiw commit

--- app/src/main/java/com/example/mytune/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/mytune/fragments/ProfileFragment.java
@@ -1,5 +1,31 @@
 package com.example.mytune.fragments;
 
-public class ProfileFragment {
-    // Hi
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mytune.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ProfileFragment extends ComposeFragment{
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        return;
+    }
 }
