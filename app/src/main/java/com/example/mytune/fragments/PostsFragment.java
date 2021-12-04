eb255670d60bb950ba0fd429282dcc0de3a17a69
tan200224 <tan200224@gmail.com>
2021-12-04 18:05:10 -0500

    Add post items

--- app/src/main/java/com/example/mytune/fragments/PostsFragment.java
+++ app/src/main/java/com/example/mytune/fragments/PostsFragment.java
@@ -9,20 +9,25 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import com.example.mytune.Post;
+import com.example.mytune.PostsAdapter;
 import com.example.mytune.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PostsFragment extends Fragment {
 
     private RecyclerView rvPosts;
-    public static final String TAG = "PostFragment";
+    public static final String TAG = "PostsFragment";
+    private PostsAdapter adapter;
+    private List<Post> allPosts;
 
     @Nullable
     @Override
@@ -32,12 +37,17 @@ public class PostsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view,savedInstanceState);
         rvPosts = view.findViewById(R.id.rvPosts);
-        queryPosts();
+        allPosts = new ArrayList<>();
         //create the adapter
+        adapter = new PostsAdapter(getContext(), allPosts);
         //create the data source
-
+        //set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
         //set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
 
     }
 
@@ -45,6 +55,7 @@ public class PostsFragment extends Fragment {
 
         //create a container that store the data from Post
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
         query.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
@@ -53,9 +64,10 @@ public class PostsFragment extends Fragment {
                     return;
                 }
                 for (Post post : posts) {
-                    Log.i(TAG, "Post " + post.getDescription());
+                    Log.i(TAG, "Post " + post.getDescription() + ", username: " + post.getUser().getString("username"));
                 }
-
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
             }
         });
 
